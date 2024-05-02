f725068de3e35585bccc0400e7e6e6d79527bfd2
Itsme29124 <144485994+Itsme29124@users.noreply.github.com>
2024-05-02 16:47:00 +0300

    Add files via upload

---
+++ assets/mywork_data.js
@@ -0,0 +1,36 @@
+import img_1 from "../assets/image.jpg"
+import img_2 from "../assets/temp2.png"
+import img_3 from "../assets/temp3.jpg"
+import img_4 from "../assets/temp4.jpg"
+import img_5 from "../assets/temp5.webp"
+
+
+const mywork_data=[
+    {
+        w_no:1,
+        w_name:"Decora Gallery - webd",
+        w_img:img_1
+    },
+    {
+        w_no:2,
+        w_name:"College Site - webd",
+        w_img:img_2
+    },
+    {
+        w_no:3,
+        w_name:"bank form - webd",
+        w_img:img_3
+    },
+    {
+        w_no:4,
+        w_name:"P2P App - Comp Networks",
+        w_img:img_4
+    },
+    {
+        w_no:5,
+        w_name:"AI/ML Gesture Recognisation",
+        w_img:img_5
+    }
+];
+
+export default mywork_data;
---
+++ assets/services_data.js
@@ -0,0 +1,33 @@
+const Services_Data = [
+    {
+        s_no: "01",
+        s_name: "Web design",
+        s_desc: "Web design involves creating visually appealing and functional websites tailored to clients' needs and preferences."
+    },
+    {
+        s_no: "02",
+        s_name: "Graphics design",
+        s_desc: "Graphics design focuses on creating engaging visual content, including logos, illustrations, and branding materials."
+    },
+    {
+        s_no: "03",
+        s_name: "Social Media",
+        s_desc: "Social media services involve managing and optimizing social media platforms to enhance brand presence and engagement."
+    },
+    {
+        s_no: "04",
+        s_name: "App design",
+        s_desc: "App design involves creating user-friendly and intuitive mobile applications that meet specific usability and design standards."
+    },
+    {
+        s_no: "06",
+        s_name: "Content Writing",
+        s_desc: "Content writing involves creating high-quality and engaging written content for websites, blogs, and marketing materials."
+    },
+    {
+        s_no: "08",
+        s_name: "UI/UX Design",
+        s_desc: "UI/UX design focuses on creating intuitive and user-friendly interfaces for digital products and applications."
+    }
+];
+export default Services_Data;
